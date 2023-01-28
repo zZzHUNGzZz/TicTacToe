6d05d8d1eda3ea891cbcc88f7c64d05e3d618a3b
Dao Duc Hung <109301166+zZzHUNGzZz@users.noreply.github.com>
2023-01-28 11:35:22 +0700

    Add files via upload

---
+++ Tic Tac Toe/Tic Tac Toe/Form1.Designer.cs
@@ -0,0 +1,367 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbName = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.clock = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 120);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(500, 500);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.Control;
+            this.btn9.FlatAppearance.BorderSize = 4;
+            this.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn9.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold);
+            this.btn9.Location = new System.Drawing.Point(335, 335);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(160, 160);
+            this.btn9.TabIndex = 8;
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.Control;
+            this.btn8.FlatAppearance.BorderSize = 4;
+            this.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn8.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold);
+            this.btn8.Location = new System.Drawing.Point(169, 335);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(160, 160);
+            this.btn8.TabIndex = 7;
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.Control;
+            this.btn7.FlatAppearance.BorderSize = 4;
+            this.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn7.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold);
+            this.btn7.Location = new System.Drawing.Point(3, 335);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(160, 160);
+            this.btn7.TabIndex = 6;
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.Control;
+            this.btn6.FlatAppearance.BorderSize = 4;
+            this.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn6.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold);
+            this.btn6.Location = new System.Drawing.Point(335, 169);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(160, 160);
+            this.btn6.TabIndex = 5;
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.Control;
+            this.btn5.FlatAppearance.BorderSize = 4;
+            this.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn5.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold);
+            this.btn5.Location = new System.Drawing.Point(169, 169);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(160, 160);
+            this.btn5.TabIndex = 4;
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.Control;
+            this.btn4.FlatAppearance.BorderSize = 4;
+            this.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn4.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold);
+            this.btn4.Location = new System.Drawing.Point(3, 169);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(160, 160);
+            this.btn4.TabIndex = 3;
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.Control;
+            this.btn3.FlatAppearance.BorderSize = 4;
+            this.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn3.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold);
+            this.btn3.Location = new System.Drawing.Point(335, 3);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(160, 160);
+            this.btn3.TabIndex = 2;
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.Control;
+            this.btn2.FlatAppearance.BorderSize = 4;
+            this.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn2.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold);
+            this.btn2.Location = new System.Drawing.Point(169, 3);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(160, 160);
+            this.btn2.TabIndex = 1;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.Control;
+            this.btn1.FlatAppearance.BorderSize = 4;
+            this.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn1.Font = new System.Drawing.Font("Arial", 45F, System.Drawing.FontStyle.Bold);
+            this.btn1.Location = new System.Drawing.Point(3, 3);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(160, 160);
+            this.btn1.TabIndex = 0;
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F);
+            this.label1.Location = new System.Drawing.Point(54, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Turn : ";
+            // 
+            // lbName
+            // 
+            this.lbName.AutoSize = true;
+            this.lbName.Font = new System.Drawing.Font("Arial", 18F);
+            this.lbName.Location = new System.Drawing.Point(129, 76);
+            this.lbName.Name = "lbName";
+            this.lbName.Size = new System.Drawing.Size(27, 27);
+            this.lbName.TabIndex = 2;
+            this.lbName.Text = "X";
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnNewGame.FlatAppearance.BorderSize = 2;
+            this.btnNewGame.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewGame.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnNewGame.Location = new System.Drawing.Point(277, 12);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(92, 44);
+            this.btnNewGame.TabIndex = 3;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = false;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 18F);
+            this.label3.Location = new System.Drawing.Point(333, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 27);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Time : ";
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("Arial", 18F);
+            this.lbTime.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lbTime.Location = new System.Drawing.Point(408, 76);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(38, 27);
+            this.lbTime.TabIndex = 5;
+            this.lbTime.Text = "60";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnExit.FlatAppearance.BorderSize = 2;
+            this.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnExit.Location = new System.Drawing.Point(403, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(92, 44);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnStart.FlatAppearance.BorderSize = 2;
+            this.btnStart.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStart.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(92, 44);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnPause.FlatAppearance.BorderSize = 2;
+            this.btnPause.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.btnPause.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPause.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnPause.Location = new System.Drawing.Point(143, 12);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(92, 44);
+            this.btnPause.TabIndex = 8;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // clock
+            // 
+            this.clock.Interval = 1000;
+            this.clock.Tick += new System.EventHandler(this.time_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 18F);
+            this.label2.Location = new System.Drawing.Point(441, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 27);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "s";
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(507, 616);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lbName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.HelpButton = true;
+            this.Name = "TicTacToe";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game Tic Tac Toe";
+            this.Load += new System.EventHandler(this.TicTacToe_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbName;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbTime;
+        private System.Windows.Forms.Timer time;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Timer clock;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tic Tac Toe/Tic Tac Toe/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class TicTacToe : Form
+    {
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        string Player = "X";
+        int Draw = 0;
+        int count = 60;
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            int t = 60 - Convert.ToInt16(lbTime.Text);
+            Button btn = (Button)sender;
+            btn.Enabled = false;
+            btn.BackColor = Color.Orange;
+
+            if (Player == "X") 
+            {
+                btn.Text = Player;
+                Player = "O";
+                lbName.Text = "O";
+            }
+            else
+            {
+                btn.Text = Player;
+                Player = "X";
+                lbName.Text = "X";
+            }
+
+            if ((btn1.Text == "X" && btn2.Text == "X" && btn3.Text == "X" && btn.Text != "") ||
+               (btn4.Text == "X" && btn5.Text == "X" && btn6.Text == "X" && btn.Text != "") ||
+               (btn7.Text == "X" && btn8.Text == "X" && btn9.Text == "X" && btn.Text != "") ||
+               (btn1.Text == "X" && btn4.Text == "X" && btn7.Text == "X" && btn.Text != "") ||
+               (btn2.Text == "X" && btn5.Text == "X" && btn8.Text == "X" && btn.Text != "") ||
+               (btn3.Text == "X" && btn6.Text == "X" && btn9.Text == "X" && btn.Text != "") ||
+               (btn1.Text == "X" && btn5.Text == "X" && btn9.Text == "X" && btn.Text != "") ||
+               (btn3.Text == "X" && btn5.Text == "X" && btn7.Text == "X" && btn.Text != ""))
+            {
+                clock.Stop();
+                btnPause.Enabled = false;
+                btnStart.Enabled = false;
+                tableLayoutPanel1.Enabled = false;
+                MessageBox.Show("Player X Win.\n\nTime : " + t + "s");
+            }
+            else if ((btn1.Text == "O" && btn2.Text == "O" && btn3.Text == "O" && btn.Text != "") ||
+                    (btn4.Text == "O" && btn5.Text == "O" && btn6.Text == "O" && btn.Text != "") ||
+                    (btn7.Text == "O" && btn8.Text == "O" && btn9.Text == "O" && btn.Text != "") ||
+                    (btn1.Text == "O" && btn4.Text == "O" && btn7.Text == "O" && btn.Text != "") ||
+                    (btn2.Text == "O" && btn5.Text == "O" && btn8.Text == "O" && btn.Text != "") ||
+                    (btn3.Text == "O" && btn6.Text == "O" && btn9.Text == "O" && btn.Text != "") ||
+                    (btn1.Text == "O" && btn5.Text == "O" && btn9.Text == "O" && btn.Text != "") ||
+                    (btn3.Text == "O" && btn5.Text == "O" && btn7.Text == "O" && btn.Text != "")) 
+            {
+                clock.Stop();
+                btnPause.Enabled = false;
+                btnStart.Enabled = false;
+                tableLayoutPanel1.Enabled = false;
+                MessageBox.Show("Player O Win.\n\nTime : " + t + "s");
+            }
+            else if(Draw == 8)
+            {
+                clock.Stop();
+                btnPause.Enabled = false;
+                btnStart.Enabled = false;
+                tableLayoutPanel1.Enabled = false;
+                MessageBox.Show("Draw !!!.\n\nTime : " + t + "s");
+            }
+            Draw++;
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            Player = "X";
+            Draw = 0;
+            count = 60;
+            lbTime.Text = "60";
+            clock.Stop();
+            btnStart.Enabled = true;
+            btnPause.Enabled = false;
+            btn1.Enabled = true; btn1.Text = ""; btn1.BackColor = Color.White;
+            btn2.Enabled = true; btn2.Text = ""; btn2.BackColor = Color.White;
+            btn3.Enabled = true; btn3.Text = ""; btn3.BackColor = Color.White;
+            btn4.Enabled = true; btn4.Text = ""; btn4.BackColor = Color.White;
+            btn5.Enabled = true; btn5.Text = ""; btn5.BackColor = Color.White;
+            btn6.Enabled = true; btn6.Text = ""; btn6.BackColor = Color.White;
+            btn7.Enabled = true; btn7.Text = ""; btn7.BackColor = Color.White;
+            btn8.Enabled = true; btn8.Text = ""; btn8.BackColor = Color.White;
+            btn9.Enabled = true; btn9.Text = ""; btn9.BackColor = Color.White;
+            tableLayoutPanel1.Enabled = false;
+        }
+
+        private void time_Tick(object sender, EventArgs e)
+        {
+            count--;
+            lbTime.Text = count.ToString();
+            if (count == 0) 
+            {
+                clock.Stop();
+                MessageBox.Show("Time out !!!");
+                btnPause.Enabled = false;
+                tableLayoutPanel1.Enabled = false;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnPause.Enabled = true;
+            clock.Start();
+            tableLayoutPanel1.Enabled = true;
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            clock.Stop();
+            tableLayoutPanel1.Enabled = false;
+            DialogResult dlr = MessageBox.Show("Restart game ?", "Game stop !!!", MessageBoxButtons.OK);
+            if (dlr == DialogResult.OK) 
+            {
+                clock.Start();
+                tableLayoutPanel1.Enabled = true;
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TicTacToe_Load(object sender, EventArgs e)
+        {
+            btnPause.Enabled = false;
+            tableLayoutPanel1.Enabled = false;
+        }
+    }
+}
